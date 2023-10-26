0066d857bbc6b19d87246c05483c9b7126194279
gabs-dev <gabrielfelipe1458@gmail.com>
2023-10-26 11:31:58 -0300

    Trabalho finalizado.

--- Chat.cs
+++ Chat.cs
@@ -6,11 +6,12 @@ using System.Text;
 namespace ChatLinguagensDiferentes {
     public partial class Chat : Form 
     {
-        private Socket socket;
+        private Thread _threadReceberMensagem;
+        private SocketService _socketService;
 
         public Chat() {
             InitializeComponent();
-            InitializeSocket();
+            _socketService = new SocketService();
         }
 
         private void Form1_Load(object sender, EventArgs e) {
@@ -26,54 +27,16 @@ namespace ChatLinguagensDiferentes {
             }
 
             // Conecta ao servidor
-            socket.Connect(enderecoIPServidor, 6969);
+            _socketService.ConnectToServer(enderecoIPServidor);
 
-            if (!socket.Connected)
+            if (!_socketService.ServerSocket.Connected)
                 ShowMessageBox("Erro ao conectar!", "Não foi possível conectar com o servidor.", MessageBoxIcon.Error);
 
             ShowMessageBox("Sucesso!", "Conectado.", MessageBoxIcon.Information);
-
-            // Inicia uma thread para receber mensagens
-            new Thread(() => {
-                while (true) 
-                {
-                    // Recebendo uma mensagem
-                    byte[] bytes = new byte[2048];
-                    socket.Receive(bytes);
-
-                    // Convertendo a mensagem em string
-                    string mensagem = Encoding.UTF8.GetString(bytes).Trim();
-
-                    // Exibe a mensagem
-                    //TextBoxHistorico.Text += $"[Pedro]: {mensagem}\n";
-                    AdicionarMensagem($"[Pedro]: {mensagem}", TipoMensagem.RECEIVED);
-                }
-            }).Start();
         }
 
         private void ButtonEnviarMensagem_Click(object sender, EventArgs e) {
             EnviarMensagem();
-        }
-
-        private string RecuperarEnderecoIPLocal() {
-
-            // Obtém a lista de todas as interfaces de rede da máquina.
-            var networkInterfaces = NetworkInterface.GetAllNetworkInterfaces().ToList();
-
-            // Filtra as interfaces de rede que têm o tipo "Wireless80211"
-            var networkInterface = networkInterfaces.Where(x => x.NetworkInterfaceType == NetworkInterfaceType.Wireless80211 && x.Name == "Wi-Fi").FirstOrDefault();
-
-            if (networkInterface == null)
-                throw new Exception("Erro ao recuperar o endereço IP da márquina.");
-
-
-            // Recupera o endereço IPv4 da rede Wi-Fi
-            var ipv4 = networkInterface.GetIPProperties().UnicastAddresses.Where(x => x.Address.AddressFamily == AddressFamily.InterNetwork).FirstOrDefault();
-
-            if (ipv4 == null)
-                throw new Exception("Endereço IPv4 não encontrado");
-
-            return ipv4.Address.ToString();
         }
 
         private void ShowMessageBox(string title, string message, MessageBoxIcon icon) {
@@ -81,20 +44,38 @@ namespace ChatLinguagensDiferentes {
             MessageBox.Show(message, title, buttons, icon);
         }
 
-        private void TextBoxHistorico_TextChanged(object sender, EventArgs e) {
-            // Mantém o foco no final do texto
-            TextBoxHistorico.SelectionStart = TextBoxHistorico.Text.Length;
-            TextBoxHistorico.ScrollToCaret();
+        private void InitListenerThread() {
+            if (_threadReceberMensagem == null) {
+                _threadReceberMensagem = new Thread(ReceberMensagem);
+                _threadReceberMensagem.Start();
+            }
+
+            if (!_threadReceberMensagem.IsAlive)
+                _threadReceberMensagem.Start();
         }
 
-        private void InitializeSocket() {
-            // Cria um socket TCP
-            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private void ReceberMensagem() {
+            while (true) {
+                try {
+                    Socket clientSocket = _socketService.ListenerSocket.Accept();
 
-            var enderecoIPLocal = IPAddress.Parse(RecuperarEnderecoIPLocal());
+                    // Recebendo uma mensagem
+                    byte[] bytes = new byte[2048];
+                    int bytesRead = clientSocket.Receive(bytes);
 
-            // Define a porta local e o endereço IP
-            socket.Bind(new IPEndPoint(enderecoIPLocal, 6969));
+                    // Convertendo a mensagem em string
+                    string mensagem = Encoding.UTF8.GetString(bytes).Trim();
+
+                    // Exibe a mensagem
+                    AdicionarMensagem($"[Pedro]: {mensagem}", TipoMensagem.RECEIVED);
+
+                    clientSocket.Close();
+                } catch (SocketException e) {
+                    Console.WriteLine(e.ToString());
+                } catch (ObjectDisposedException e) {
+                    Console.WriteLine(e.ToString());
+                }
+            }
         }
 
         private void AdicionarMensagem(string mensagem, TipoMensagem tipo) {
@@ -107,19 +88,22 @@ namespace ChatLinguagensDiferentes {
             else if (tipo == TipoMensagem.RECEIVED)
                 corNomeUsuario = Color.DarkOrange;
 
-            TextBoxHistorico.SelectionStart = TextBoxHistorico.TextLength;
-            TextBoxHistorico.SelectionLength = 0;
+            TextBoxHistorico.Invoke((MethodInvoker)delegate {
+                TextBoxHistorico.SelectionStart = TextBoxHistorico.TextLength;
+                TextBoxHistorico.SelectionLength = 0;
 
-            TextBoxHistorico.SelectionColor = corNomeUsuario;
-            TextBoxHistorico.SelectionFont = new Font(TextBoxHistorico.Font, FontStyle.Bold);
-            TextBoxHistorico.AppendText(usuario + ": ");
+                TextBoxHistorico.SelectionColor = corNomeUsuario;
+                TextBoxHistorico.SelectionFont = new Font(TextBoxHistorico.Font, FontStyle.Bold);
+                TextBoxHistorico.AppendText(usuario + ": ");
 
-            TextBoxHistorico.SelectionColor = Color.Black;
-            TextBoxHistorico.SelectionFont = new Font(TextBoxHistorico.Font, FontStyle.Regular);
-            TextBoxHistorico.AppendText(conteudoMensagem + Environment.NewLine);
+                TextBoxHistorico.SelectionColor = Color.Black;
+                TextBoxHistorico.SelectionFont = new Font(TextBoxHistorico.Font, FontStyle.Regular);
+                TextBoxHistorico.AppendText(conteudoMensagem);
+                TextBoxHistorico.AppendText(Environment.NewLine);
 
-            TextBoxHistorico.SelectionStart = TextBoxHistorico.TextLength;
-            TextBoxHistorico.ScrollToCaret();
+                TextBoxHistorico.SelectionStart = TextBoxHistorico.TextLength;
+                TextBoxHistorico.ScrollToCaret();
+            });
         }
 
         private void TextBoxMensagem_KeyPress(object sender, KeyPressEventArgs e) {
@@ -132,29 +116,33 @@ namespace ChatLinguagensDiferentes {
         }
 
         private void EnviarMensagem() {
-            if (socket == null || !socket.Connected) {
-                ShowMessageBox("Erro de conexão!", "Nenhuma conexão via socket foi inicializada. " +
-                    "Primeiro estabeleça conexão com o outro participante.", MessageBoxIcon.Error);
-                return;
-            }
-
             string mensagem = TextBoxMensagem.Text;
 
             if (mensagem.Trim().Length == 0)
                 return;
 
+            if (_socketService.ServerSocket == null || !_socketService.ServerSocket.Connected) {
+                ShowMessageBox("Erro de conexão!", "Nenhuma conexão via socket foi inicializada. " +
+                    "Primeiro estabeleça conexão com o outro participante.", MessageBoxIcon.Error);
+                return;
+            }
+
             // Envia a mensagem
-            socket.Send(Encoding.UTF8.GetBytes(mensagem));
+            _socketService.SendMessage(mensagem);
 
             // Adiciona a minha mensagem ao histórico e limpa e campo de mensagem
             AdicionarMensagem($"[Eu]: {mensagem}", TipoMensagem.SENT);
             TextBoxMensagem.Text = "";
             TextBoxMensagem.Focus();
+
+            InitListenerThread();
         }
 
         private void Chat_FormClosing(object sender, FormClosingEventArgs e) {
-            if (socket != null)
-                socket.Close();
+            _socketService.CloseAllConnections();
+
+            if (_threadReceberMensagem != null)
+                _threadReceberMensagem.Interrupt();
 
             Console.WriteLine("Encerrando a aplicação...");
         }
---
+++ SocketService.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatLinguagensDiferentes {
+    public class SocketService {
+
+        public Socket ServerSocket { get; set; }
+
+        public Socket ListenerSocket { get; set; }
+
+        public int ServerPort { get; set; }
+
+        public int ClientPort { get; set; }
+
+        public string ServerAddress { get; set; }
+
+        public SocketService() {
+            ServerPort = 6969;
+            ClientPort = 6970;
+            InitializeServerSocket();
+            InitializeListenerSocket();
+        }
+
+        public void ConnectToServer(string serverAddress) {
+            if (ServerSocket != null && ServerSocket.Connected)
+                return;
+
+            if (ServerSocket == null)
+                throw new Exception("Nenhuma instância do Socket do servidor criada.");
+
+            ServerSocket.Connect(serverAddress, ServerPort);
+
+            this.ServerAddress = serverAddress;
+        }
+
+        public void SendMessage(string message) {
+            if (!ServerSocket.Connected)
+                throw new Exception("O servidor não está conectado.");
+
+            ServerSocket.Send(Encoding.UTF8.GetBytes(message));
+        }
+
+        private void InitializeServerSocket() {
+            ServerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+
+        public void CloseAllConnections() {
+            if (ServerSocket != null)
+                ServerSocket.Close();
+
+            if (ListenerSocket != null)
+                ListenerSocket.Close();
+        }
+
+        private void InitializeListenerSocket() {
+            ListenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            var localIPAddress = GetLocalIPAddress();
+
+            // Define a porta local e o endereco IP
+            ListenerSocket.Bind(new IPEndPoint(localIPAddress, 6970));
+
+            // Comeca a ouvir por conexoes de clientes. O valor 10 é o número máximo de conexões pendentes.
+            ListenerSocket.Listen(10);
+        }
+
+        private IPAddress GetLocalIPAddress() {
+
+            // Obtém a lista de todas as interfaces de rede da maquina.
+            var networkInterfaces = NetworkInterface.GetAllNetworkInterfaces().ToList();
+
+            // Filtra as interfaces de rede que têm o tipo "Wireless80211"
+            var networkInterface = networkInterfaces.Where(x => x.NetworkInterfaceType == NetworkInterfaceType.Wireless80211 && x.Name == "Wi-Fi").FirstOrDefault();
+
+            if (networkInterface == null)
+                throw new Exception("Erro ao recuperar o endereço IP da márquina.");
+
+
+            // Recupera o endereço IPv4 da rede Wi-Fi
+            var ipv4 = networkInterface.GetIPProperties().UnicastAddresses.Where(x => x.Address.AddressFamily == AddressFamily.InterNetwork).FirstOrDefault();
+
+            if (ipv4 == null)
+                throw new Exception("Endereço IPv4 não encontrado");
+
+            return ipv4.Address;
+        }
+    }
+}
